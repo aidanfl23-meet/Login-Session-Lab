ef598fff8223117a187ef964fa064f8943f8af7a
aidanfl23-meet <93254417+aidanfl23-meet@users.noreply.github.com>
2022-08-01 16:14:58 +0300

    Done

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask import session as login_session
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supers-secret-key'
+
+@app.route('/', methods=['GET', 'POST'] ) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'GET':
+		return render_template('home.html')
+	else:
+		quote = request.form["quote"]
+		name = request.form["name"]
+		age = request.form["age"]
+		login_session["quote"] = quote
+		login_session["name"] = name
+		login_session["age"] = age 		
+		return redirect(url_for('display'))
 
 
 @app.route('/error')
@@ -18,15 +27,13 @@ def error():
 
 @app.route('/display')
 def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html' , logi = login_session) # What variables are needed?
 
 
 @app.route('/thanks')
 def thanks():
-
 	return render_template('thanks.html')
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(port=8329, debug=True)
